7178d244542a91bfadc33d83960cf59bf9c1ece1
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2026-01-21 20:49:31 +0100

    create updateFromDto method in BookMapper and use it in update method in BookService

--- src/main/java/mate/academy/onlinebookstore/service/BookServiceImpl.java
+++ src/main/java/mate/academy/onlinebookstore/service/BookServiceImpl.java
@@ -39,12 +39,10 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto update(Long id, CreateBookRequestDto requestDto) {
-        bookRepository.findById(id)
+        Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Can't find book by id: " + id));
-        Book book = bookMapper.toModel(requestDto);
-        book.setId(id);
-        bookRepository.save(book);
-        return bookMapper.toDto(book);
+        bookMapper.updateBookFromDto(requestDto, book);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
