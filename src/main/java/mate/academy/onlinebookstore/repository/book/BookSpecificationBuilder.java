141dd0ea1191df5dc0267de72d119befaa7f42c4
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2026-01-26 20:44:30 +0100

    Fix checkstyle issues in BookSpecificationBuilder

--- src/main/java/mate/academy/onlinebookstore/repository/book/BookSpecificationBuilder.java
+++ src/main/java/mate/academy/onlinebookstore/repository/book/BookSpecificationBuilder.java
@@ -14,16 +14,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
-    private final  SpecificationProviderManager<Book> bookSpecificationProviderManager;
+    private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
 
     @Override
     public Specification<Book> build(BookSearchParametersDto searchParams) {
         Specification<Book> spec = Specification.allOf();
-        if (searchParams != null && searchParams.author() != null && searchParams.author().length > 0) {
+        if (searchParams != null && searchParams.author() != null
+                && searchParams.author().length > 0) {
             spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(AUTHOR)
                     .getSpecification(searchParams.author()));
         }
-        if (searchParams != null && searchParams.title() != null && searchParams.title().length > 0) {
+        if (searchParams != null && searchParams.title() != null
+                && searchParams.title().length > 0) {
             spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(TITLE)
                     .getSpecification(searchParams.title()));
         }
