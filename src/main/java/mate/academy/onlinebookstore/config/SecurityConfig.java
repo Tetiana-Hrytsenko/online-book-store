b2463b7546547bc6d712d918c69e535d4ac073d3
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2026-03-06 17:02:33 +0100

    Fix:create constants for literals, replace addFilterAfter() to addFilterBefore()
    Fix:create constants for literals, replace addFilterAfter() to addFilterBefore()
    
    refactor SecurityConfig

--- src/main/java/mate/academy/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/mate/academy/onlinebookstore/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableMethodSecurity
 @RequiredArgsConstructor
@@ -46,8 +46,8 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterAfter(jwtAuthenticationFilter,
-                        SecurityContextHolderFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
     }
